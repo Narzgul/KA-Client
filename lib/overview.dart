0b70d58db4bf19e3af7fc162f5f6f605f893432e
Narzgul <tzoockeer@gmail.com>
2024-03-06 15:06:24 +0100

    make ui watch changes in the selected products

--- lib/overview.dart
+++ lib/overview.dart
@@ -2,47 +2,43 @@ import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
 import 'package:ka_client/product.dart';
 import 'package:ka_client/style_components/product_info_dialog.dart';
+import 'package:watch_it/watch_it.dart';
 
 import 'api_connection.dart';
 
-class Overview extends StatelessWidget {
-  final Future<List<Product>> products = GetIt.I<APIConnection>().fetchProducts();
+class Overview extends WatchingWidget {
+  final Future<List<Product>> products =
+      GetIt.I<APIConnection>().fetchProducts();
 
   Overview({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder(
-      builder: (context, snapshot) {
-        if (snapshot.hasData && snapshot.data != null) {
-          List<Product> products = snapshot.data!; // Can't be null
-          return ListView.builder(
-            itemCount: products.length,
-            padding: const EdgeInsets.fromLTRB(8, 8, 8, 75),
-            itemBuilder: (context, index) {
-              return ListTile(
-                title: Text(products[index].title),
-                subtitle: Text(products[index].location),
-                trailing: Text(products[index].price.toString()),
-                onTap: () {
-                  showDialog(
-                    context: context,
-                    builder: (context) {
-                      return ProductInfoDialog(product: products[index]);
-                    },
-                  );
-                }
-              );
-            },
-          );
-        } else if (snapshot.hasData && snapshot.data == null) {
-          return const Center(child: Text('No data'));
-        } else if (snapshot.hasError) {
-          return Center(child: Text('${snapshot.error}'));
-        }
-        return const Center(child: CircularProgressIndicator());
+    List<Product> products = watchFuture<APIConnection, List<Product>>(
+      (p0) => p0.fetchProducts(),
+      initialValue: [],
+    ).data!;
+    if (products.isEmpty) {
+      return const Center(child: CircularProgressIndicator());
+    }
+    return ListView.builder(
+      itemCount: products.length,
+      padding: const EdgeInsets.fromLTRB(8, 8, 8, 75),
+      itemBuilder: (context, index) {
+        return ListTile(
+          title: Text(products[index].title),
+          subtitle: Text(products[index].location),
+          trailing: Text(products[index].price.toString()),
+          onTap: () {
+            showDialog(
+              context: context,
+              builder: (context) {
+                return ProductInfoDialog(product: products[index]);
+              },
+            );
+          },
+        );
       },
-      future: products,
     );
   }
 }
--- lib/price_chart_page.dart
+++ lib/price_chart_page.dart
@@ -2,31 +2,26 @@ import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
 import 'package:ka_client/product.dart';
 import 'package:ka_client/style_components/price_chart.dart';
+import 'package:watch_it/watch_it.dart';
 
 import 'api_connection.dart';
 
-class PriceChartPage extends StatelessWidget {
-  final Future<List<Product>> products = GetIt.I<APIConnection>().fetchProducts();
+class PriceChartPage extends WatchingWidget {
+  final Future<List<Product>> products =
+      GetIt.I<APIConnection>().fetchProducts();
 
   PriceChartPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder(
-      builder: (context, snapshot) {
-        if (snapshot.connectionState != ConnectionState.done) {
-          return const Center(child: CircularProgressIndicator());
-        }
-        if (snapshot.data != null) {
-          List<Product> products = snapshot.data!; // Can't be null
-          return PriceChart(prices: products.map((e) => e.price).toList());
-        }
-        if (snapshot.hasError) {
-          return Center(child: Text('${snapshot.error}'));
-        }
-        return const Center(child: Text('No data'));
-      },
-      future: products,
-    );
+    List<Product> products = watchFuture<APIConnection, List<Product>>(
+      (p0) => p0.fetchProducts(),
+      initialValue: [],
+    ).data!;
+    if (products.isEmpty) {
+      return const Center(child: CircularProgressIndicator());
+    }
+
+    return PriceChart(prices: products.map((e) => e.price).toList());
   }
 }
